9e9c906547ec2126ea806599d6064cef8c5a706b
nexus-ai-tta <nexus.ai.tta@gmail.com>
2025-05-21 19:28:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ word_index = imdb.get_word_index()
 reverse_word_index = {value: key for key, value in word_index.items()}
 
 # Load the pre-trained model with ReLU activation
-model = load_model('simple_rnn_imdb.h5', compile=False)
+model = load_model("simple_rnn_imdb.h5")
+model.save("simple_rnn_imdb_converted.keras")
 
 
 # Step 2: Helper Functions
